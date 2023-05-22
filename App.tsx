9ef144491da79e19f58e40e7c15e68274a263e47
Bernard Braun da Silva <bernard.sbraun@gmail.com>
2023-05-22 16:48:07 -0300

    First commit in project

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet } from 'react-native';
+
+
+function App() {
+  return <>
+    <SafeAreaView style={styles.screen}>
+      
+    </SafeAreaView>
+  </>
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+  }
+});
+
+export default App; 
