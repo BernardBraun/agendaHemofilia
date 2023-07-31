0de590e7e8ff78d4ef07dc30d75a6a2d78b699b2
Bernard Braun da Silva <bernard.sbraun@gmail.com>
2023-07-31 11:30:39 -0300

    Function: Home screen  - Created welcome message with both cases (have or not data).  - Adjusted baseUrls file to add homescreen API url.
    Bernard Braun

--- src/screens/Home/components/TopComponent.tsx
+++ src/screens/Home/components/TopComponent.tsx
@@ -1,81 +1,85 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Image, Text, View, StyleSheet, Alert } from 'react-native';
 import logo from '../../../assets/LogoDiario.png';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { URL_HOMESCREEN } from '../../../helper/baseUrl';
 
 class TopComponent extends React.Component {
-
     constructor(props) {
-        super(props);
-        this.state = {
-            userName: '',
-            lastInfusionDate: ''
-        };
+      super(props);
+      this.state = {
+        userName: '',
+        lastInfusionDate: '',
+      };
     }
-
+  
     async componentDidMount() {
-        try {
-            // Ao montar o componente, busca o email salvo no AsyncStorage
-            const userEmail = await AsyncStorage.getItem('userEmail');
-            const token = await AsyncStorage.getItem('token');
-            if (userEmail && token) {
-                //criar chamada usando token e email
-                const url = 'https://api.adviceslip.com/advice';
-
-                fetch(url, {
-                    method: "GET",
-                    // headers: {
-                    //     "Content-Type": "application/json",
-                    // },
-                    // body: JSON.stringify(formData),
-                })
-                    .then((response) => {
-                        if (!response.ok) {
-                            throw new Error('Erro na requisição. Status code: ' + response.status);
-                        }
-                        return response.json()
-                    })
-                    .then((data) => {
-                        try {
-                            // Alert.alert("Teste", data.slip.advice)
-                            console.log(data.slip.advice)
-                            this.setState({ userName: data.slip.id, lastInfusionDate: data.slip.advice });
-                        } catch (error) {
-                            Alert.alert("Erro", "Favor tente novamente mais tarde.")
-                        }
-                    })
-                //utilizar o json para informar userName e lastInfusionDate
-
-                //this.setState({ userName: data.userName, lastInfusionDate: data.lastInfusionDate });
-
-                // this.setState({ userName: "Theo", lastInfusionDate: new Date().toString() });
-
-            }
-        } catch (error) {
-            // Trate qualquer erro que possa ocorrer durante a busca
-            console.log('Erro ao buscar o email do usuário:', error);
+      try {
+        const userEmail = await AsyncStorage.getItem('userEmail');
+        const token = await AsyncStorage.getItem('token');
+  
+        if (userEmail && token) {
+          const url = URL_HOMESCREEN + '/' + userEmail;
+          console.log(url)
+  
+          fetch(url, {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': 'Bearer ' + token
+            },
+          })
+            .then((response) => {
+              if (!response.ok) {
+                throw new Error('Erro na requisição. Código de status: ' + response.status);
+              }
+              return response.json();
+            })
+            .then((data) => {
+              try {
+                this.setState({ userName: data.userName, lastInfusionDate: data.lastInfusionDate });
+              } catch (error) {
+                Alert.alert('Erro', 'Favor tente novamente mais tarde.');
+              }
+            })
+            .catch((error) => {
+              console.log('Erro na requisição:', error);
+              Alert.alert('Erro', 'Favor tente novamente mais tarde.');
+            });
         }
+      } catch (error) {
+        console.log('Erro ao buscar o email do usuário:', error);
+      }
     }
-
+    
+    
     render() {
-        const { userName } = this.state;
-        const { lastInfusionDate } = this.state;
+        console.log(this.state)
+        const { userName, lastInfusionDate } = this.state;
+  
+      let message;
 
-
-        return <SafeAreaView style={styles.container}>
-            <View style={styles.headerWelcome}>
-                <Image source={logo} style={styles.image} />
-                <Text style={styles.welcomeText}>Bem vindo {'\n'} {userName}</Text>
-            </View>
-            <View style={styles.adviceView}>
-                <Text style={styles.adviceText}>Sua última infusão foi no dia: {lastInfusionDate}</Text>
-            </View>
-            <View style={styles.lineBreak} />
-
+      if (lastInfusionDate != null) {
+        message = 'Sua última infusão foi no dia: ' + lastInfusionDate;
+      } else {
+        message = 'Você ainda não tem infusões registradas.';
+      }
+  
+      return (
+        <SafeAreaView style={styles.container}>
+          <View style={styles.headerWelcome}>
+            <Image source={logo} style={styles.image} />
+            <Text style={styles.welcomeText}>Bem vindo {'\n'} {userName}</Text>
+          </View>
+          <View style={styles.adviceView}>
+            <Text style={styles.adviceText}>{message}</Text>
+          </View>
+          <View style={styles.lineBreak} />
         </SafeAreaView>
+      );
     }
-}
+  }
 
 const styles = StyleSheet.create({
     container: {
